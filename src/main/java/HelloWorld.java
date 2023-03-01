4d3873a819cb3c21c661ab976e07da2e43a554bb
João Lopes <2212561@my.ipleiria.pt>
2023-03-01 13:41:00 +0000

    João Lopes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 
 public class HelloWorld extends JFrame {
     private JPanel panel1;
+    private JLabel lblMensagem2;
+
     public HelloWorld(){
         setContentPane(panel1);
+        lblMensagem2.setText("O João Lopes esteve aqui");
         pack();
     }
     public static void main(String[] args) {
